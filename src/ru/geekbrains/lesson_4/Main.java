c2e4d3de369c696ef3bb56fe0d758fedd49320ec
Travis <suporal@mail.ru>
2020-10-22 17:39:08 +0300

    начало

--- src/ru/geekbrains/lesson_4/Main.java
+++ src/ru/geekbrains/lesson_4/Main.java
@@ -1,8 +1,18 @@
 package ru.geekbrains.lesson_4;
 
-public class Main {
+public class Main
+{
+    static char[][] map;
+    static final int SIZE = 3;
+    static final int DOTS_TO_VIN = 3;
 
-    public static void main(String[] args) {
-	// write your code here
+    static final char DOT_EMPTY = ' ';
+    static final char DOT_X = 'X';
+    static final char DOT_0 = '0';
+
+    public static void main(String[] args)
+    {
+
+
     }
 }
